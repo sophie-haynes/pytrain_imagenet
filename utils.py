daf8cf4fe0a1b27ee763596e43395573f5c9047b
sophie-haynes <s.c.haynes@rgu.ac.uk>
2024-11-09 17:12:44 +0000

    Fix Utils Syntax Error

--- utils.py
+++ utils.py
@@ -128,7 +128,7 @@ class MetricLogger:
             data_time.update(time.time() - end)
             yield obj
             iter_time.update(time.time() - end)
-            if i % print_freq == 0 && i != 0:
+            if i % print_freq == 0 and i != 0:
                 eta_seconds = iter_time.global_avg * (len(iterable) - i)
                 eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
                 if torch.cuda.is_available():
